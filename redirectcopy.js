d274e77fce57f4b504225d9c0c2a662e869786aa
giahoangland <101549691+giahoangland@users.noreply.github.com>
2022-03-14 17:09:01 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://thanglongluxurygiahoangland.blogspot.com/'>https://thanglongluxurygiahoangland.blogspot.com/</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://giahoangland.com/'>https://giahoangland.com/</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
